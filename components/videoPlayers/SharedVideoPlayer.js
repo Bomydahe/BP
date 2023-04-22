40336b48a05ce06d3de706a71e5af6f4bcbc14fd
Bomydahe <duranik.rastislav@gmail.com>
2023-04-22 13:21:05 +0200

    icons

--- components/videoPlayers/SharedVideoPlayer.js
+++ components/videoPlayers/SharedVideoPlayer.js
@@ -351,17 +351,16 @@ const styles = StyleSheet.create({
 
   overlaySvg: {
     position: "absolute",
-    top: -15,
-    left: -5,
-
+    top: 10,
+    left: 0,
     zIndex: 2,
     width: "100%",
     height: "100%",
   },
 
   modalContainer: {
-    position: "absolute",
-    top: 0,
+    right: 10,
+    bottom: 0,
     left: 0,
     right: 0,
     bottom: 0,
