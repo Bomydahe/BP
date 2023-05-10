c0d0b8a29cfae1a55c4b50f98746affe6c41e40e
Rastislav Duránik <63494659+Bomydahe@users.noreply.github.com>
2023-05-10 11:59:03 +0200

    Update Category.js

--- components/Category.js
+++ components/Category.js
@@ -1,3 +1,20 @@
+/*
+  * Author: Rastislav Duránik (xduran03)
+  * File: Category.js
+  * Brief: 
+     Component displays a list of videos in a grid layout.
+     It receives the videos as a prop, and renders each 
+     video thumbnail as an Image component within a 
+     TouchableOpacity. When a user taps on a video thumbnail,
+     it navigates to the VideoPlayer screen with the selected
+     video's URL as a parameter. The component uses a FlatList
+     to display the videos in a grid layout with two columns.
+     The width of each video is calculated based on the device
+     width and the number of columns. The component also defines
+     some styles for the container, the flatlist, and the video 
+     thumbnails.
+*/
+
 import React from "react";
 import { Video } from "expo-av";
 import {
