2141f31e3a021d2f9e01191ef96e24f9b96fa5f3
Rastislav Duránik <63494659+Bomydahe@users.noreply.github.com>
2023-05-10 12:00:16 +0200

    Update FAB.js

--- components/FAB.js
+++ components/FAB.js
@@ -1,3 +1,18 @@
+/*
+  * Author: Rastislav Duránik (xduran03)
+  * File: FAB.js
+  * Brief: 
+     Component that displays a FAB (Floating Action Button)
+     with a group of actions when clicked. The actions 
+     include adding a video, adding a category, and comparing 
+     videos. The component receives several props: open and setOpen
+     to handle the open state of the FAB, pickVideo to handle picking 
+     a video, navigate to navigate to a different screen, addCategory
+     to handle adding a category, categories to compare videos, and
+     handleLogout to handle logging out of the app. The FAB has a 
+     blue background color and white icons.
+*/
+
 import React from "react";
 import { FAB } from "react-native-paper";
 
